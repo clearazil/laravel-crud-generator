b1200eac73ff893090acc2c599b28bb9a9a5284b
Derk van der Heide <derkvanderheide@hotmail.com>
2021-08-09 15:54:41 +0200

    project init

---
+++ packages/laravel-crud-generator/src/Providers/LaravelCrudGeneratorServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Clearazil\LaravelCrudGenerator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCrudGeneratorProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
